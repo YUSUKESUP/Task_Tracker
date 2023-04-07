121308866020b43388555e13601ee69b0bb7e484
YUUSKE <yusuke.1173ab@gmail.com>
2023-04-07 23:18:58 +0900

    test

--- lib/components/month_summary.calender.dart
+++ lib/components/month_summary.calender.dart
@@ -1,77 +1,39 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
-//
-//
-// class MonthlySummaryCalemder extends StatelessWidget {
-//
-//   final Map<DateTime, int>? heatmapDatasets;
-//   final int value;
-//
-//   MonthlySummaryCalemder({
-//     super.key,// import 'package:flutter/material.dart';
-// // import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
-// //
-// //
-// // class MonthlySummaryCalemder extends StatelessWidget {
-// //
-// //   final Map<DateTime, int>? heatmapDatasets;
-// //   final int value;
-// //
-// //   MonthlySummaryCalemder({
-// //     super.key,
-// //     required this.heatmapDatasets,
-// //     required this.value,
-// //   });
-// //
-// //   @override
-// //   Widget build(BuildContext context) {
-// //
-// //     return Container(
-// //       padding: const EdgeInsets.only(bottom: 15),
-// //       child: HeatMapCalendar(
-// //         weekTextColor: Colors.black,
-// //         textColor: Colors.black,
-// //         showColorTip: true,
-// //         colorMode: ColorMode.opacity,
-// //         datasets:heatmapDatasets ,
-// //         colorsets: const {
-// //             1: Colors.redAccent,
-// //         },
-// //       onClick: (value) {
-// //         ScaffoldMessenger.of(context)
-// //             .showSnackBar(SnackBar(content: Text(value.toString())));
-// //       },
-// //       ),
-// //     );
-// //   }
-// // }
-// //
-// //
-//     required this.heatmapDatasets,
-//     required this.value,
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//
-//     return Container(
-//       padding: const EdgeInsets.only(bottom: 15),
-//       child: HeatMapCalendar(
-//         weekTextColor: Colors.black,
-//         textColor: Colors.black,
-//         showColorTip: true,
-//         colorMode: ColorMode.opacity,
-//         datasets:heatmapDatasets ,
-//         colorsets: const {
-//             1: Colors.redAccent,
-//         },
-//       onClick: (value) {
-//         ScaffoldMessenger.of(context)
-//             .showSnackBar(SnackBar(content: Text(value.toString())));
-//       },
-//       ),
-//     );
-//   }
-// }
-//
-//
+import 'package:flutter/material.dart';
+import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
+
+
+class MonthlySummaryCalemder extends StatelessWidget {
+
+  final Map<DateTime, int>? heatmapDatasets;
+  final int value;
+
+  MonthlySummaryCalemder({
+    super.key,
+    required this.heatmapDatasets,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      padding: const EdgeInsets.only(bottom: 15),
+      child: HeatMapCalendar(
+        weekTextColor: Colors.black,
+        textColor: Colors.black,
+        showColorTip: true,
+        colorMode: ColorMode.opacity,
+        datasets:heatmapDatasets ,
+        colorsets: const {
+            1: Colors.redAccent,
+        },
+      onClick: (value) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(value.toString())));
+      },
+      ),
+    );
+  }
+}
+
+
