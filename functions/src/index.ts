b9c87c4ef1a29227dd1c36981389f3c2744f987d
YUUSKE <yusuke.1173ab@gmail.com>
2023-04-18 15:12:53 +0900

    軽微な修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,38 +1,41 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
-admin.initializeApp(); // initializeApp()の位置を修正
-
-const db = admin.firestore();
+admin.initializeApp(functions.config().firebase);
 
 // 3分ごとに実行
 export const timer = functions.pubsub.schedule("*/3 * * * *").onRun(async (context) => {
   try {
     functions.logger.info("timer start", {structuredData: true});
-    // 通知を送信するトークンを取得
-    // const fcmTokensSnapshot = await admin.firestore().collection("users").get();
-    // const fcmTokens = fcmTokensSnapshot.docs.map((doc) => doc.data().fcmToken);
-    // 通知を送信する処理
+
     const message = {
       notification: {
         title: "テスト", // 通知のタイトル
         body: "テスト", // 通知の本文
-      }, // 通知を送信するトークンの配列
+      },
     };
-    const usersRef = db.collection("users");
-    const snapshot = await usersRef.get();
-    if (snapshot.empty) {
-      console.log("No matching user");
-      return;
+
+    const db = admin.firestore();
+    const collectionRef = db.collection("users");
+    const querySnapshot = await collectionRef.get();
+    const queryDocSnapshot = querySnapshot.docs;
+    const tokens = [];
+    for (const snapshot of queryDocSnapshot) {
+      if (snapshot.data().fcmToken) {
+        tokens.push(snapshot.data().fcmToken);
+      }
     }
-    snapshot.forEach((doc) => {
-      // push notify
-      const token = doc.data()["fcmToken"];
-      admin.messaging().sendToDevice(token, message);
-    }); // FCM で通知を送信
+
+    const multicastMessage = {
+      tokens: tokens,
+      notification: message.notification,
+    };
+    await admin.messaging().sendMulticast(multicastMessage); // FCM で通知を送信
     return null;
   } catch (error) {
     console.error("Error in timer:", error);
     return null;
   }
 });
+
+
